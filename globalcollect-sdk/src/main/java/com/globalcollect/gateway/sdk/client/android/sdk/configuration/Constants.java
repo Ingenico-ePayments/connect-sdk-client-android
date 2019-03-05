373a2d23ce26313b70699c797c05d07fcddf7453
Ingenico ePayments <jenkins@isaac.nl>
2019-03-05 16:24:18 +0100

    Release 4.5.2.

--- globalcollect-sdk/src/main/java/com/globalcollect/gateway/sdk/client/android/sdk/configuration/Constants.java
+++ globalcollect-sdk/src/main/java/com/globalcollect/gateway/sdk/client/android/sdk/configuration/Constants.java
@@ -7,7 +7,7 @@ package com.globalcollect.gateway.sdk.client.android.sdk.configuration;
 public class Constants {
 
 	/** SDK version **/
-	public final static String SDK_IDENTIFIER = "AndroidClientSDK/v4.5.1";
+	public final static String SDK_IDENTIFIER = "AndroidClientSDK/v4.5.2";
 
 	/** SDK creator **/
 	public final static String SDK_CREATOR = "Ingenico";
