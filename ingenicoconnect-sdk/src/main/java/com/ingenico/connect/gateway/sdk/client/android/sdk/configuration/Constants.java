0303cc0fc4fcc51abc32f292f99575d31e32e14f
Ingenico ePayments <github@epay.ingenico.com>
2023-10-16 16:15:08 +0200

    Release 6.1.3.

--- ingenicoconnect-sdk/src/main/java/com/ingenico/connect/gateway/sdk/client/android/sdk/configuration/Constants.java
+++ ingenicoconnect-sdk/src/main/java/com/ingenico/connect/gateway/sdk/client/android/sdk/configuration/Constants.java
@@ -7,7 +7,7 @@ package com.ingenico.connect.gateway.sdk.client.android.sdk.configuration;
 public class Constants {
 
 	/** SDK version **/
-	public final static String SDK_IDENTIFIER = "AndroidClientSDK/v6.1.2";
+	public final static String SDK_IDENTIFIER = "AndroidClientSDK/v6.1.3";
 
 	/** SDK creator **/
 	public final static String SDK_CREATOR = "Ingenico";
